1fc71481763d0a74420474e98e9e540d05cd5acc
kenanalic20 <kenan.alic@edu.fit.ba>
2026-04-21 12:06:16 +0200

    Fix

--- firmware/sketch_mar10a/sketch_mar10a.ino
+++ firmware/sketch_mar10a/sketch_mar10a.ino
@@ -1,5 +1,5 @@
 #include <DHT.h>
-#include <WiFi.h>
+#include<WiFi.h>
 #include <FirebaseESP32.h>
 #include "secrets.h"
 
@@ -13,56 +13,54 @@ FirebaseConfig config;
 FirebaseAuth auth;
 FirebaseJson json;
 
-const int gasThreshold = 1000;
+const int gasThreshold = 1500;
 const int fanSpinDuration = 10000;
-
-volatile bool readFlag = false;   
 
 int gas = 0;
 float temperature = 0;
 float humidity = 0;
 
-bool fanSpinActive = false;
-unsigned long fanStartTime = 0;
-unsigned long lastSend = 0;
-
+volatile bool fanSpinActive = false;
+volatile unsigned long fanStartTime = 0;
 hw_timer_t *timer = NULL;
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void IRAM_ATTR onTimer() {
-  readFlag = true;
+  gas = analogRead(MQ135);
+  if (gas > gasThreshold) {
+    digitalWrite(fanPin, HIGH);
+    fanSpinActive = true;
+    fanStartTime = millis();
+  }
 }
+
+
 
 void setup() {
   pinMode(fanPin, OUTPUT);
   pinMode(MQ135, INPUT);
   dht.begin();
   Serial.begin(115200);
-
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting WiFi");
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nConnected!");
-
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
   auth.user.email = EMAIL;
   auth.user.password = PASSWORD;
-
-  Firebase.begin(&config, &auth);
+  Firebase.begin(&config,&auth);
   Firebase.reconnectWiFi(true);
-
+  
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmWrite(timer, 1000, true);
   timerAlarmEnable(timer);
 }
-
 void sendUpTime() {
   json.clear();
 
@@ -70,41 +68,43 @@ void sendUpTime() {
   json.set("humidity", humidity);
   json.set("gas", gas);
 
+  Serial.println("Preparing to send data to Firebase...");
+
+  String jsonStr;
+  json.toString(jsonStr, true); 
+
   if (Firebase.setJSON(firebaseData, "/esp32", json)) {
     Serial.println("Firebase OK");
   } else {
     Serial.println("Firebase FAILED");
-    Serial.println(firebaseData.errorReason().c_str());
+    String err = firebaseData.errorReason().c_str();
+    Serial.println(err);
   }
 }
-
 void loop() {
+  delay(1000);
+  Serial.print("Gas: ");
+  Serial.println(gas);
+  delay(1000);  //for debug
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("DHT read failed!");
+  } else {
+    Serial.print("Temp: ");
+    Serial.println(temperature);
 
-  if (readFlag) {
-    readFlag = false;
-
-    gas = analogRead(MQ135);
-    temperature = dht.readTemperature();
-    humidity = dht.readHumidity();
-
-    Serial.print("Gas: "); Serial.println(gas);
-    Serial.print("Temp: "); Serial.println(temperature);
-    Serial.print("Humidity: "); Serial.println(humidity);
-
-    if (gas > gasThreshold && !fanSpinActive) {
-      digitalWrite(fanPin, HIGH);
-      fanSpinActive = true;
-      fanStartTime = millis();
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+  }
+  sendUpTime();
+  if (fanSpinActive) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - fanStartTime >= fanSpinDuration) {
+      digitalWrite(fanPin, LOW);
+      fanSpinActive = false;
     }
   }
-
-  if (fanSpinActive && millis() - fanStartTime >= fanSpinDuration) {
-    digitalWrite(fanPin, LOW);
-    fanSpinActive = false;
-  }
-
-  if (millis() - lastSend > 1000) {
-    lastSend = millis();
-    sendUpTime();
-  }
+ 
+  
 }
